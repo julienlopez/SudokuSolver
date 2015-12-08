e5074d34ff5ab87d25beabd097f6a8d17b97e201
julien lopez <julien.lopez51@gmail.com>
2015-12-08 18:06:41 +0100

    setting up a basic class to compile everything

---
+++ lib/grille.cpp
@@ -0,0 +1,8 @@
+#include "grille.hpp"
+
+namespace SudokuSolver
+{
+
+
+
+} //SudokuSolver
---
+++ lib/grille.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+namespace SudokuSolver
+{
+
+    class Grille
+    {
+
+    };
+
+} //SudokuSolver
